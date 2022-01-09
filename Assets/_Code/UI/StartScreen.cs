c20707060d3621ae78d9c411c3e090f59fd46a81
tutan09 <andrespino.95@gmail.com>
2022-01-09 18:00:16 +0100

    escape working in startscreen

--- Assets/_Code/UI/StartScreen.cs
+++ Assets/_Code/UI/StartScreen.cs
@@ -1,9 +1,12 @@
+using TutanDev.References;
+using UnityEditor;
 using UnityEngine;
 
 namespace TutanDev.UI
 {
     public class StartScreen : MonoBehaviour
     {
+        [SerializeField] BoolReference exitReference;
         TopScores topScores;
 
         public void StartClick()
@@ -15,6 +18,20 @@ namespace TutanDev.UI
         {
             topScores = GetComponentInChildren<TopScores>();
             topScores.Init(SaveSystem.SaveSystem.LoadGame());
+        }
+
+        private void Update()
+        {
+            if (!exitReference.value) return;
+
+            if(Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                EditorApplication.ExitPlaymode();
+            }
+            else
+            {
+                Application.Quit();
+            }
         }
     }
 }
